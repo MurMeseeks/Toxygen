ae74a9c9d4fb64578ef1445c3f15dfe71cb53a74
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-04 20:24:25 +0300

    :package: Update installing scriptw

---
+++ install.sql
@@ -0,0 +1,56 @@
+use z2Ymbo1JnT;
+CREATE TABLE `users` (
+                         `user_id` INT NOT NULL AUTO_INCREMENT,
+                         `username` VARCHAR(30) NOT NULL UNIQUE,
+                         `password` VARCHAR(512) NOT NULL,
+                         `isAdmin` BOOLEAN NOT NULL,
+                         `phone` VARCHAR(30),
+                         `email` VARCHAR(30) NOT NULL,
+                         `address` VARCHAR(30),
+                         `birthday` VARCHAR(30),
+                         PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `orders` (
+                          `order_id` INT NOT NULL AUTO_INCREMENT,
+                          `product_id` INT NOT NULL ,
+                          `qty` INT NOT NULL,
+                          `user_id` INT NOT NULL,
+                          `order_time` TIMESTAMP NOT NULL,
+                          PRIMARY KEY (`order_id`,`product_id`)
+);
+
+CREATE TABLE `products` (
+                            `product_id` INT NOT NULL AUTO_INCREMENT,
+                            `product_name` TEXT NOT NULL,
+                            `price` FLOAT NOT NULL,
+                            `description` TEXT NOT NULL,
+                            `picture_link` TEXT NOT NULL,
+                            `weight` FLOAT NOT NULL,
+                            `color` INT NOT NULL,
+                            `width` FLOAT NOT NULL,
+                            `height` FLOAT NOT NULL,
+                            `quantity` INT NOT NULL,
+                            PRIMARY KEY (`product_id`)
+);
+
+CREATE TABLE `Categories` (
+                              `product_id` INT NOT NULL,
+                              `category_id` INT NOT NULL,
+                              PRIMARY KEY (`product_id`,`category_id`)
+);
+
+CREATE TABLE `reviews` (
+                           `review_id` INT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(30) NOT NULL,
+                           `rating` INT NOT NULL,
+                           `review` TEXT NOT NULL,
+                           `userdick` TEXT NOT NULL,
+                           PRIMARY KEY (`review_id`)
+);
+
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`);
+
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
+
+ALTER TABLE `Categories` ADD CONSTRAINT `Categories_fk0` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`);
