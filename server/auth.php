c22fb7b5940e34a7275f3a8e71b9805d68217872
Dzhabrail <dzhabulya@gmail.com>
2019-05-05 18:10:02 +0300

    Fixed path

--- server/auth.php
+++ server/auth.php
@@ -5,7 +5,7 @@ print_r($_POST);
 if (isset($_POST['submit']) and $_POST['submit'] == 'Войти' and isset($_POST['password']) and isset($_POST['username'])) {
 	if ($res = generic_read("users", "username" . " = " . wrap_single_quotes($_POST["username"]))) {
 		if (hash("whirlpool", $_POST['password']) == $res['password']) {
-			print "zaebis";
+            header('Location: ../src/index.html');
 		}
 	} else {
 		print "no such user or password"; //todo one ,ore fucking todo
