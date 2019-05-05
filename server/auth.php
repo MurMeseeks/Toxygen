0ba888ef7ce7183db5a1a4b2fd353ce60a341947
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-05 17:03:46 +0300

    :lock: Added auth moduole

--- server/auth.php
+++ server/auth.php
@@ -1,1 +1,13 @@
 <?php
+include("sql_manage.php");
+include('sql_misc.php');
+print_r($_POST);
+if ($_POST['submit'] == 'Войти') {
+	if ($res = generic_read("users", "username" . " = " . $_POST["username"])) {
+		print_r($res);
+	}
+	else
+	{
+		print "no such user or password";
+	}
+}
