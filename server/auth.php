48df999bb6c06ccaf0a3d0233d9a49ac9219e7b2
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-05 18:54:34 +0300

    :lock: registration finished

--- server/auth.php
+++ server/auth.php
@@ -7,11 +7,13 @@ if (isset($_SESSION['logged']) && $_SESSION['logged'] == $_POST['username']) {
 } elseif (isset($_POST['submit']) and $_POST['submit'] == 'Войти' and isset($_POST['password']) and isset($_POST['username'])) {
 	if ($res = generic_read("users", "username" . " = " . wrap_single_quotes($_POST["username"]))) {
 		if (hash("whirlpool", $_POST['password']) == $res['password']) {
-            header('Location: ../src/index.html');
-			print "zaebis";
+			header('Location: ../src/index.html');
 			$_SESSION['logged'] = $_POST['username'];
-		}
-		else
+			if (isset($res['isAdmin']) and $res['isAdmin'] = true)
+				$_SESSION['admin'] = true;
+			else
+				$_SESSION['admin'] = false;
+		} else
 			print "bad user or password";
 	} else {
 		print "bad user or password"; //todo one ,ore fucking todo
